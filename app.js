264d945b57fd8da617f073662511cf0f61efc27c
Bhavik2209 <bhavikrohit22@gmail.com>
2024-02-05 21:09:09 +0530

    add

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const port = 3000
+const bodyParser = require("body-parser");
 const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-
+    res.send("hello")
 })
 app.listen(port,()=>{
     console.log("server is running")
